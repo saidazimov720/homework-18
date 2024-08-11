54a9cebc9971ebd39e8d0871f76dc67d6a49f877
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-08-11 17:44:05 +0500

    detect

---
+++ script.js
@@ -0,0 +1,5 @@
+const userN = document.getElementById("name").value;
+const userW = document.getElementById("weapon").value;
+const userP = document.getElementById("photo").value;
+const computerNames = ["SubZero", "Scorpion", "Kitana"];
+const computerWeapons = ["sword", "axe", "bow"];
